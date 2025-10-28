f3b6f1e386d844948e7f543d37a25334970f20ca
HarumaTetsukawa <kmc2530@kamiyama.ac.jp>
2025-10-28 13:26:01 +0900

    Update dote.rb

--- dote.rb
+++ dote.rb
@@ -3,7 +3,7 @@ class Dote < Formula
   desc "dot e command"
   homepage "https://github.com/IamGODofNEWWORLD/homebrew-dote"
   url "https://raw.githubusercontent.com/IamGODofNEWWORLD/homebrew-dote/main/dote.c"
-  sha256 "26fbd111e15f825d163cdccf05d2ea8cf344018476a139d2969e9c150d6d7208"
+  sha256 "69ce57f0914d99912d1feba8eefa2bf58aa91a53b1257ae512079e1438b712ec"
   version "1.0.0"
 
   def install
